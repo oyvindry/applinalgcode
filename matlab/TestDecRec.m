99077bcb8d35031c2416a8cc3e9635ef109020b3
Øyvind Ryan <oyvindry@math.uio.no>
2017-02-05 10:50:04 +0100

    renaming

--- matlab/TestDecRec.m
+++ matlab/TestDecRec.m
@@ -139,22 +139,22 @@ classdef TestDecRec < matlab.unittest.TestCase
             x = testCase.x;
             eps = testCase.eps;
             nres = testCase.nres;
-            z = IDWTImpl(DWTImpl(x, nres, wave_name, 0, 0), nres, wave_name, 0, 0);
+            z = dwt_impl(dwt_impl(x, nres, wave_name, 1, 1, 0, 0), nres, wave_name, 0, 1, 0, 0);
             testCase.verifyTrue(norm(z-x,2) < eps);
-            z = IDWTImpl(DWTImpl(x, nres, wave_name, 1, 0), nres, wave_name, 1, 0);
+            z = dwt_impl(dwt_impl(x, nres, wave_name, 1, 1, 1, 0), nres, wave_name, 0, 1, 1, 0);
             testCase.verifyTrue(norm(z-x,2) < eps);
-            z = IDWTImpl(DWTImpl(x, nres, wave_name, 0, 1), nres, wave_name, 0, 1);
+            z = dwt_impl(dwt_impl(x, nres, wave_name, 1, 1, 0, 1), nres, wave_name, 0, 1, 0, 1);
             testCase.verifyTrue(norm(z-x,2) < eps);
-            z = IDWTImpl(DWTImpl(x, nres, wave_name, 1, 1), nres, wave_name, 1, 1);
+            z = dwt_impl(dwt_impl(x, nres, wave_name, 1, 1, 1, 1), nres, wave_name, 0, 1, 1, 1);
             testCase.verifyTrue(norm(z-x,2) < eps);
         end
         function testWaveletDecRecBoundary(testCase, wave_name)
             x = testCase.x;
             eps = testCase.eps;
             nres = testCase.nres;
-            z = IDWTImpl(DWTImpl(x, nres, wave_name, 2, 0), nres, wave_name, 2, 0);
+            z = dwt_impl(dwt_impl(x, nres, wave_name, 1, 1, 2, 0), nres, wave_name, 0, 1, 2, 0);
             testCase.verifyTrue(norm(z-x,2) < eps);
-            z = IDWTImpl(DWTImpl(x, nres, wave_name, 3, 0), nres, wave_name, 3, 0);
+            z = dwt_impl(dwt_impl(x, nres, wave_name, 1, 1, 3, 0), nres, wave_name, 0, 1, 3, 0);
             testCase.verifyTrue(norm(z-x,2) < eps);
         end
     end
--- matlab/showDWT.m
+++ matlab/showDWT.m
@@ -1,6 +1,6 @@
 function showDWT(m, wave_name, lowres)
     img = double(imread('images/lena.png', 'png'));
-    img = DWT2Impl(img, m, wave_name);
+    img = dwt_impl(img, m, wave_name, 1, 2);
     if lowres
         M =size(img, 1); N = size(img, 2);
         tokeep=img(1:(M/(2^m)), 1:(N/(2^m)), :);
@@ -11,7 +11,7 @@ function showDWT(m, wave_name, lowres)
         sz(1) = sz(1)/2^m; sz(2) = sz(2)/2^m;
         img(1:sz(1), 1:sz(2), :) = 0;
     end
-    img = IDWT2Impl(img, m, wave_name);
+    img = dwt_impl(img, m, wave_name, 0, 2);
     imshow(uint8(255*mapto01(img)));
   
   
