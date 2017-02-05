85bab476182ba52b9777d275a354c1bcd8dd6bf2
Øyvind Ryan <oyvindry@math.uio.no>
2017-02-05 10:49:40 +0100

    renaming

--- matlab/forw_comp_rev_DWT.m
+++ matlab/forw_comp_rev_DWT.m
@@ -6,11 +6,11 @@ function [x,fs]=forw_comp_rev_DWT(m, wave_name, lr)
     [x, fs] = audioread('sounds/castanets.wav');
     N = 2^17;
     x = x(1:N);
-    x = DWTImpl(x, m, wave_name);
+    x = dwt_impl(x, m, wave_name);
     if lowres==1
         x((N/2^m+1):end) = 0;
     else
         x(1:(N/2^m)) = 0;
     end
-    x = IDWTImpl(x, m, wave_name);
+    x = dwt_impl(x, m, wave_name, 0);
     x = x/max(max(abs(x)));
