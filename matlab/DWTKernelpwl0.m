a6db33ea351c6c811259e25dc1e8aa1ac9845e4e
Øyvind Ryan <oyvindry@math.uio.no>
2017-02-06 22:00:02 +0100

    deleted old files

--- matlab/DWTKernelpwl0.m
+++
@@ -1,8 +0,0 @@
-function x = DWTKernelpwl0(x, bd_mode, dual)
-    if dual
-        x = x/sqrt(2);
-        x = liftingstepevensymm(0.5, x, bd_mode);
-    else
-        x = x*sqrt(2);
-        x = liftingstepoddsymm(-0.5, x, bd_mode);
-    end
