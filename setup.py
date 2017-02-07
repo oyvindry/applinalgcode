dd585ff19357f5d29a56c10eed9c2497657761a4
Vegard Antun <vegarant@student.matnat.uio.no>
2017-02-07 18:28:36 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(name='Applinalgcode',
+      version='1.0',
+      description='Fourier and wavelets module',
+      author='Oyvind Ryan',
+      author_email='oyvindry@math.uio.no',
+      url='https://github.com/oyvindry/applinalgcode',
+      packages=['applinalg'],
+      package_dir={'applinalg': 'python'},
+      #package_data={'applinalg': ['data/images/lena.png', 'data/sounds/castanets.wav']}
+     );
+
+
