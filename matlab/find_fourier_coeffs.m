c89c8830cceec85b74f5702acc6aad44ed608897
Øyvind Ryan <oyvindry@math.uio.no>
2018-08-13 21:19:00 +0200

    changes

--- matlab/find_fourier_coeffs.m
+++
@@ -1,8 +0,0 @@
-function [ank,bnk] = find_fourier_coeffs(n, k, T)
-    ank=0; bnk=0;
-    if k > 0
-        [ankprev,bnkprev] = find_fourier_coeffs(n, k-1, T)
-        ank = -k*T*bnkprev/(2*pi*n);
-        bnk = -T^k/(pi*n) + k*T*ankprev/(2*pi*n);
-    end
-end
